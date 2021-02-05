ff3395811bb449a0ce1bbef68cefe757389e5c35
JNR23 <junior_bhzte@hotmail.com>
2021-02-05 14:50:41 -0300

    Stylizing Boxing Destinations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(function () {
+    //MENU
     $('.nav-toggle, .nav-close').click(function (e) {
         e.preventDefault();
         $('.nav').toggleClass('active');
     }); 
+
+    //FIXED HEADER
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.header').addClass('fixed');
+        } else {
+            $('.header').removeClass('fixed');
+        }
+    })
 });
